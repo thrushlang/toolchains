057c66a70a4a75fb608aa4d85826d2f4a96f0d7d
DevCheckOG <ing.kevin.benavides@gmail.com>
2025-11-26 11:07:09 -0600

    `FIX` Enhancing compressing.

--- targets/x86_64-pc-windows-libcmt/build.bat
+++ targets/x86_64-pc-windows-libcmt/build.bat
@@ -24,15 +24,9 @@ cmake .. %LLVM_CMAKE_CONFIGURE_FLAGS%
 cmake --build . %CMAKE_BUILD_FLAGS%
 cmake --build . --target install %CMAKE_BUILD_FLAGS%
 
-for %%f in ("bin\*.exe") do (
-    if /i not "%%~nxf" == "llvm-config.exe" (
-        del "%%f"
-    )
-)
-
 cd %THIS_DIR%
 
-7z a -tzip %GITHUB_WORKSPACE%\%LLVM_RELEASE_FILE% %LLVM_RELEASE_NAME%
+7z a -tzip -mx=9 %GITHUB_WORKSPACE%\%LLVM_RELEASE_FILE% %LLVM_RELEASE_NAME%
 
 goto :eof
 
@@ -48,7 +42,7 @@ cmake --build . --target install %CMAKE_BUILD_FLAGS%
 
 cd %THIS_DIR%
 
-7z a -tzip %GITHUB_WORKSPACE%\%CLANG_RELEASE_FILE% %CLANG_RELEASE_NAME%
+7z a -tzip -mx=9 %GITHUB_WORKSPACE%\%CLANG_RELEASE_FILE% %CLANG_RELEASE_NAME%
 
 goto :eof
 
